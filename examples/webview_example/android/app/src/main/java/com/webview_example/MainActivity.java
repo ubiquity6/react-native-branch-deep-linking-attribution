dac78568c48d9c3010133e90f1f958f1e3b591ec
Jimmy Dee <jgvdthree@gmail.com>
2019-04-26 14:46:10 -0700

    Set intent data to null in onResume

--- examples/webview_example/android/app/src/main/java/com/webview_example/MainActivity.java
+++ examples/webview_example/android/app/src/main/java/com/webview_example/MainActivity.java
@@ -24,6 +24,14 @@ public class MainActivity extends ReactActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+
+        // clear intent data
+        getIntent().setData(null);
+    }
+
+    @Override
     public void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
         setIntent(intent);
